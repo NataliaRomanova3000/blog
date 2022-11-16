c57d4986f85ca79edd866c63eee32fb2cece6d64
Natalya Romanova <natalya.romanova@zurich.com>
2022-11-16 16:08:42 +0100

    form group

--- src/app/pages/guestbook/components/new-record-dialog/new-record-dialog.component.ts
+++ src/app/pages/guestbook/components/new-record-dialog/new-record-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Guestbook } from 'src/app/models/guestbook';
 import { Store } from '@ngrx/store';
@@ -15,26 +15,29 @@ export class NewRecordDialogComponent {
 
   constructor(
     private dialogRef: MatDialogRef<NewRecordDialogComponent>,
-    private store: Store<GuestbookState>
+    private store: Store<GuestbookState>,
+    private formBuilder: FormBuilder
   ) { }
 
-  author = new FormControl('', [Validators.required]);
-  message = new FormControl('', [Validators.required, Validators.minLength(20)]);
+  messageForm: FormGroup= this.formBuilder.group({
+    author: [null, Validators.required],
+    message: [null, [Validators.required, Validators.minLength(20)]]
+  });
 
   getErrorMessage() {
-    return this.author.hasError('required') ? 'You must enter a name or email' : '';
+    return this.messageForm.controls.author.errors.required ? 'You must enter a name or email' : '';
   }
 
   getErrorMessageMessage() {
-    return this.message.hasError('required') ? 'You must enter a message' :
-          (this.message.errors?.minlength ? 'You mast provide 20 symbols at list' : '') ;
+    return this.messageForm.controls.message.errors.required ? 'You must enter a message' :
+           'You mast provide 20 symbols at list' ;
   }
 
-  save() {
-    if(this.author.valid && this.message.valid) {
+  submit() {
+    if(this.messageForm.valid ) {
       const guestbook = new Guestbook();
-      guestbook.author = this.author.value;
-      guestbook.message = this.message.value;
+      guestbook.author = this.messageForm.controls.author.value;
+      guestbook.message = this.messageForm.controls.message.value;
 
       this.store.dispatch(addGuestbook(guestbook));
       this.dialogRef.close(guestbook);
